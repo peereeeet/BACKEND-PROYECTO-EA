a8f581ad0e9afb403e6306d92a1f929659ce845c
Pedrorodriguez016 <146534432+Pedrorodriguez016@users.noreply.github.com>
2025-11-30 22:33:54 +0100

    Eliminados todos los logs

--- src/controller/eventoController.ts
+++ src/controller/eventoController.ts
@@ -171,7 +171,6 @@ export async function createEventoFromPanel(req: Request, res: Response) {
     return res.status(201).json(evento);
   } catch (err: any) {
     logger.error(`Error al crear evento desde panel: ${err?.message || err}`);
-    console.error('createEventoFromPanel', err);
     return res.status(500).json({ message: err?.message || 'No se pudo crear el evento (panel).' });
   }
 }
