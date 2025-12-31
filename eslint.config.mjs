c30aa5280b2f54b9c5cf2939d27e064975df892c
Pedrorodriguez016 <146534432+Pedrorodriguez016@users.noreply.github.com>
2025-12-31 03:01:08 +0100

    Solucionado problema en websockets

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,10 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_'
+      }],
       '@typescript-eslint/no-explicit-any': 'warn',
       'no-console': 'error',
     },
